ff38f0dd64f7e045f4cc5c5ece95ce55a66d8080
Manase Kimutai <mernasseh@gmail.com>
2025-04-10 00:31:02 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,21 @@ import user from "../data/user";
 console.log(user);
 
 function App() {
+  const user = {
+    name: "Liza",
+    city: "New York",
+    bio: "I made this!",
+    color: "firebrick",
+    links: {
+      github: "https://github.com/liza",
+      linkedin: "https://www.linkedin.com/in/liza/",
+    },
+  };
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
 }
